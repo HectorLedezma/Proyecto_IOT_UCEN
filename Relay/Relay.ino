37593088cb3d5c25b5628c058353fe3199efbaf2
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2023-10-05 01:57:49 -0300

    sensor de humedad y codigo para Wi-Fi

--- Relay/Relay.ino
+++ Relay/Relay.ino
@@ -1,9 +1,30 @@
 #include "Wire.h"
 #include "LiquidCrystal_I2C.h"
+#define SensorPin D0
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int relay1 = D3; // for ESP8266 microcontroller
 int relay2 = D4;
+
+#include <ESP8266WiFi.h>
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK "your-password"
+#endif
+
+const char* ssid = "Redmi 9C";
+const char* password = "012345678";
+
+//const char* ssid = "Zeroora";
+//const char* password = "notieneclave";
+
+const char* host = "djxmmx.net";
+const uint16_t port = 17;
+
+
 void setup() {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
   // put your setup code here, to run once:
@@ -13,9 +34,20 @@ void setup() {
 }
 
 void loop() {
+  float sensorValue = analogRead(SensorPin);
+  Serial.println(sensorValue);
+  if(sensorValue == 0.0){
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, LOW);
+  }else{
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, HIGH);
+  }
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print("Humedad: "+String(sensorValue));
   // put your main code here, to run repeatedly:
-  digitalWrite(relay1, HIGH);
-  digitalWrite(relay2, HIGH);
+  /*
   lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("1    ON");
@@ -23,12 +55,12 @@ void loop() {
   lcd.print("2    OFF");
   delay(4000);
 
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, LOW);
+  
   lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("1    OFF");
   lcd.setCursor(2, 1);
   lcd.print("2    ON");
-  delay(4000);
+  */
+  delay(2000);
 }
